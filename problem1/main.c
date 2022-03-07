5fa15166df99952588a8b344d23ee84f2b529d74
ElianaGiraldo-98 <eliana.giraldod@upb.edu.co>
2022-03-06 23:26:43 -0500

    Examen

--- problem1/main.c
+++ problem1/main.c
@@ -36,18 +36,163 @@ void printArray(struct array *parr)
 
 void getArray(struct array *parr)
 {
-    printf("Introduce el tamaño del arreglo:\n");
-    scanf("%d", &parr->size);
-    int tm=parr->size;
-    int pdata [tm];
-    for(int i=0; i<tm; i++)
+    int val;
+    char number[5];
+
+    if (fgets(number, 5, stdin) != NULL)
     {
-    	printf("\nEscriba el valor [%d] ", i);
-    	scanf("%d", &parr->pdata[i]);
-    	parr++;
-    } 
+        number[strlen(number) -1 ] = 0];
+        //printf("The string to convert is %s\n", number);
+    }
+    int successItems = sscanf(number,"%d",&val);
+    parr->pdata = malloc(sizeof(int)*parr->size);
+    for(int i=0; i<parr->size; i++)
+    {
+        if (fgets(number,5,stdin) != NULL) 
+        {
+            int successItemsd = sscanf(number, "%d", parr->pdata+I);
+
+        } 
+    }    
 }
 
 void arrayCommon(struct array *arrIn1, struct array *arrIn2, struct array *arrOut)
 {
+   if(arrIn1->size>arrIn2->size)
+    {
+        size = arrIn1->size;
+    }
+    else if(arrIn2->size>arrIn1->size)
+    {
+        size = arrIn2->size;
+    }
+    else if(arrIn2->size == arrIn1->size)
+    {
+        size = arrIn2->size;
+    }
+     int cambio[size];
+
+    for(int i = 0; vari1<size; vari1++) 
+    { 
+        successItems[i]=-1;
+    } 
+int i=0;
+
+    for(int i=0; i<arrIn1->size;i++)
+    {
+        for(int j=0; j<arrIn2->size;j++)
+        {
+            if(arrIn1->pdata[i]==arrIn2->pdata[j]) 
+            {
+                int condi = 0;
+                for(int k=0; vari3<size; k++)
+                {
+                    if(arrIn1->pdata[i]==successItemsd[k])
+                    {
+                       k = size;
+                        condi = 1;
+                    }
+                }
+                if(condi==0)
+                {
+                    successItemsd[i] = arrIn2->pdata[j];
+                    i++;
+                    arrOut->size++;
+                }
+            }
+        }
+    }
+        arrOut->pdata = malloc(sizeof(int)*arrOut->size);
+    
+    for(int i = 0; i<arrOut->size;i++)
+    {
+        *(arrOut->pdata+i) = successItemsd[i];
+    }
+}
+}
+void freeMemory(struct array *arr1, struct array *arr2, struct array *arr3)
+{
+    free(arr1->pdata);
+    free(arr1);
+
+    free(arr2->pdata);
+    free(arr2);
+
+    free(arr3->pdata);
+    free(arr3);
+}
+
+int main(void)
+{
+
+#ifdef DOLOG
+    FILE *log = fopen("log", "w");
+    if (log == NULL)
+    {
+        perror("log file creation fails: ");
+        return EXIT_FAILURE;
+    }
+#endif
+
+    char commandBuf[64];
+
+    LOG("creating an object for each array\n");
+
+    struct array *arr1 = malloc(sizeof(struct array) * 1);
+    initArray(arr1);
+    struct array *arr2 = malloc(sizeof(struct array) * 1);
+    initArray(arr2);    
+    struct array *arr3 = malloc(sizeof(struct array) * 1);
+    initArray(arr3);
+
+
+    LOG("Init command loop\n");
+
+    while (fgets(commandBuf, sizeof(commandBuf), stdin) != NULL)
+    {
+        commandBuf[strlen(commandBuf) - 1] = 0;
+
+        if (strncmp(commandBuf, "arr1", strlen("arr1")) == 0)
+        {
+            LOG("getting array1\n");
+            getArray(arr1);
+        }
+        else if (strncmp(commandBuf, "arr2", strlen("arr2")) == 0)
+        {
+            LOG("getting array2\n");
+            getArray(arr2);
+        }
+        else if (strncmp(commandBuf, "printArr1", strlen("printArr1")) == 0)
+        {
+            LOG("printing arr1\n");
+            printArray(arr1);
+        }
+        else if (strncmp(commandBuf, "printArr2", strlen("printArr2")) == 0)
+        {
+            LOG("printing arr2\n");
+            printArray(arr2);
+        }
+        else if (strncmp(commandBuf, "compare", strlen("compare")) == 0)
+        {
+            LOG("Compare arrays\n");
+            arrayCommon(arr1, arr2, arr3);
+        }
+        else if (strncmp(commandBuf, "printResult", strlen("printResult")) == 0)
+        {
+            LOG("printing compare result\n");
+            printf("%d\n", arr3->size);
+            if (arr3->size != 0)
+            {
+                printArray(arr3);
+            }
+        }
+        else if (strncmp(commandBuf, "exit", strlen("exit")) == 0)
+        {
+            LOG("running exit command\n");
+            freeMemory(arr1,arr2,arr3);
+            break;
+        }
+    }
+
+    return EXIT_SUCCESS;
 }
